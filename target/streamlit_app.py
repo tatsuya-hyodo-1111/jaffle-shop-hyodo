5f7613cc656390ba0ce357edd23ac1a89a0ae0ef
tatsuya-hyodo-1111 <tatsuya.hyodo@zdh.co.jp>
2026-03-17 00:57:32 +0000

    commit

--- target/streamlit_app.py
+++ target/streamlit_app.py
@@ -2,9 +2,6 @@ import json
 import streamlit as st
 import streamlit.components.v1 as components
 
-# HTMLファイルのパス
-html_file_path = 'static_index.html'
-
 st.set_page_config(
     page_title="dbt App",
     page_icon="🧊",
@@ -27,8 +24,25 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
-# StreamlitでHTMLを表示
-with open(html_file_path, 'r', encoding='utf-8') as file:
+# manifest.jsonとcatalog.jsonを読み込む
+with open('manifest.json', 'r', encoding='utf-8') as f:
+    manifest = json.load(f)
+
+with open('catalog.json', 'r', encoding='utf-8') as f:
+    catalog = json.load(f)
+
+# index.htmlを読み込む
+with open('index.html', 'r', encoding='utf-8') as file:
     html_content = file.read()
+
+# manifest/catalogをHTMLに埋め込む
+inject_script = f'''
+<script>
+    window.manifest = {json.dumps(manifest)};
+    window.catalog = {json.dumps(catalog)};
+</script>
+'''
+html_content = html_content.replace('</head>', inject_script + '</head>')
+
 components.html(html_content, height=1024, scrolling=False)
 
